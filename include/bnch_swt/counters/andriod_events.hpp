0f16a474a12264b0d2748579f1457ab48308e814
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2025-12-06 13:11:29 -0500

    Updating the cache_clearer impl.

--- include/bnch_swt/counters/andriod_events.hpp
+++ include/bnch_swt/counters/andriod_events.hpp
@@ -1,4 +1,26 @@
-// bnch_swt/Counters/AndroidCounters.hpp
+/*
+	MIT License
+
+	Copyright (c) 2024 RealTimeChris
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this
+	software and associated documentation files (the "Software"), to deal in the Software
+	without restriction, including without limitation the rights to use, copy, modify, merge,
+	publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+	persons to whom the Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all copies or
+	substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+	INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+	PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+	FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+	OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+	DEALINGS IN THE SOFTWARE.
+*/
+/// https://github.com/RealTimeChris/benchmarksuite
+
 #pragma once
 
 #include <bnch_swt/config.hpp>
@@ -10,16 +32,16 @@
 namespace bnch_swt::internal {
 
 	template<typename event_count, uint64_t count> struct event_collector_type<event_count, benchmark_types::cpu, count> : public std::vector<event_count> {
-		uint64_t currentIndex{};
+		uint64_t current_index{};
 
 		BNCH_SWT_HOST event_collector_type() : std::vector<event_count_t>{ count_t } {};
 
 		template<typename function_type, typename... arg_types> BNCH_SWT_HOST void run(arg_types&&... args) {
-			const auto startClock = std::chrono::high_resolution_clock::now();
-			std::vector<event_count_t>::operator[](currentIndex).bytesProcessedVal.emplace(static_cast<uint64_t>(function_type::impl(std::forward<arg_types>(args)...)));
-			const auto endClock = std::chrono::high_resolution_clock::now();
-			std::vector<event_count_t>::operator[](currentIndex).elapsed = endClock - startClock;
-			++currentIndex;
+			const auto start_clock = clock_type::now();
+			std::vector<event_count_t>::operator[](current_index).bytesProcessedVal.emplace(static_cast<uint64_t>(function_type::impl(std::forward<arg_types>(args)...)));
+			const auto end_clock = clock_type::now();
+			Vstd::vector<event_count>::operator[](current_index).elapsed_ns_val.emplace(end_clock - start_clock);
+			++current_index;
 			return;
 		}
 	};
